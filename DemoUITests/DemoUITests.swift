28f6c0f691a5ba678ab56d2f9fbacc6b11749851
paul <paul.griffin@knowit.se>
2017-06-02 13:41:55 +0200

    Simple testcase

---
+++ DemoUITests/DemoUITests.swift
@@ -0,0 +1,34 @@
+//
+//  DemoUITests.swift
+//  DemoUITests
+//
+//  Created by Paul Griffin on 2017-06-02.
+//  Copyright © 2017 Knowit. All rights reserved.
+//
+
+import XCTest
+
+class DemoUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    func testExample() {
+        
+        //Make sure the button works
+        
+        XCUIDevice.shared().orientation = .portrait
+        
+        let app = XCUIApplication()
+        app.buttons["Important button"].tap()
+        XCTAssert( app.staticTexts["Works"].exists )
+        app.navigationBars["UIView"].children(matching: .button).matching(identifier: "Back").element(boundBy: 0).tap()
+        
+        
+    
+    }
+    
+}
