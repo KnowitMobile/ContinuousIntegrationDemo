381f5f719926f04cad9ecfa4afa752db995fc746
paul <paul.griffin@knowit.se>
2017-06-05 15:22:52 +0200

    Fixed UI

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func submitTapped(_ sender: Any) {
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func validateName(name: String) -> Bool {
+        let components = name.components(separatedBy: " ")
+        
+        guard let components.count == 2 else { return false }
+        
     }
-
 
 }
 
